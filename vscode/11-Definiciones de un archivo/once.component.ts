2687a2db8e96ee475a64357c18b7b2f07b3573e9
Daniel Patricio <dpvandroid54@gmail.com>
2024-03-23 13:45:07 -0400

    Mejorar usando VSC

---
+++ vscode/11-Definiciones de un archivo/once.component.ts
@@ -0,0 +1,25 @@
+
+/*
+<!---------------------------------------------
+
+----------------------                         |
+                                     
+                                               |
+Comado para:
+                                               |
+Windows y Linux: Ctrl+shift + O 
+                                               |
+MAC:  ⌘+shift + O
+ 
+                                               |
+________________________________________________
+ -->**/
+
+
+
+
+let Genkidama=()=> {}
+let FinalFlash=()=> {}
+let Makakozapo=()=> {}
+let mafuba=()=> {}
+let revivir=(Krilin)=> {}
